bf5003b5b6edba4788e938fb1e1d5f16f8ba1ce5
sai1004 <saikiran.bavandla@gmail.com>
2025-02-25 20:10:53 +0530

    commit for virtual scroll

--- pagination-virtual-scroll/src/app/app.component.ts
+++ pagination-virtual-scroll/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { pipe, Subject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pagination-virtual-scroll';
+export class AppComponent implements OnInit, OnDestroy {
+    coinsList: any[] = [];
+    page: number = 1;
+    isLoading: boolean = false;
+    limit: number = 25;
+    isError: boolean = false;
+    destroy$ = new Subject();
+    // private observer!: IntersectionObserver;
+
+    @ViewChild('scrollAnchor', { static: false }) scrollAnchorEle!: ElementRef;
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        this.getCoinsList();
+    }
+
+    ngAfterViewInit(): void {
+        this.setupIntersectionObserver();
+    }
+
+    setupIntersectionObserver() {
+        let observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                this.getCoinsList();
+            }
+        });
+        observer.observe(this.scrollAnchorEle.nativeElement);
+    }
+
+    getCoinsList() {
+        if (this.isLoading) return;
+        this.isLoading = true;
+        this.http
+            .get(
+                `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${this.limit}2&page=${this.page}&sparkline=false`
+            )
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((newCoinsList: any) => {
+                this.coinsList = [...this.coinsList, ...newCoinsList];
+                this.page += 1;
+                this.isLoading = false;
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next('');
+        this.destroy$.complete();
+        // this.observer.disconnect() not required
+    }
 }
