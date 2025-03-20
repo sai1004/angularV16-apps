d5b01f71d5da06269a1ea8235a812b08626014a3
sai1004 <saikiran.bavandla@gmail.com>
2025-03-20 14:11:35 +0530

    commit for addon form

--- form-reactive-addonforms/src/app/app.component.ts
+++ form-reactive-addonforms/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'form-reactive-addonforms';
+export class AppComponent implements OnInit {
+    profileForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.profileForm = this.createProfileForm();
+    }
+
+    ngOnInit(): void {}
+
+    get nominations(): FormArray {
+        return this.profileForm.get('nominations') as FormArray;
+    }
+
+    createProfileForm() {
+        return this.fb.group({
+            name: ['', Validators.required],
+            email: ['', Validators.required],
+            mobile: ['', Validators.required],
+            nominations: this.fb.array([]),
+        });
+    }
+
+    createNominationsForm() {
+        return this.fb.group({
+            name: ['', Validators.required],
+            age: ['', Validators.required],
+            email: ['', Validators.required],
+            mobile: ['', Validators.required],
+            percentage: ['', Validators.required],
+        });
+    }
+
+    addNominations() {
+        this.nominations.push(this.createNominationsForm());
+    }
+
+    removeNominations(index: number) {
+        this.nominations.removeAt(index);
+    }
+
+    onSubmit() {
+        console.log(this.profileForm.value);
+    }
 }
