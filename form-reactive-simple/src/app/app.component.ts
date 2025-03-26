997bbd4f6cde31c86904940330db5b9b39cb7c8c
sai1004 <saikiran.bavandla@gmail.com>
2025-03-26 14:36:26 +0530

    commit for reactive-form-simple

--- form-reactive-simple/src/app/app.component.ts
+++ form-reactive-simple/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'form-reactive-simple';
+export class AppComponent implements OnInit {
+    profileForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.profileForm = this.createProfileForm();
+    }
+
+    ngOnInit(): void {}
+
+    createProfileForm() {
+        return this.fb.group({
+            name: ['', Validators.required],
+            email: ['', Validators.required],
+            mobile: ['', Validators.required],
+            age: ['', Validators.required],
+        });
+    }
+
+    onSubmit() {
+        if(this.profileForm.valid){
+            console.log(this.profileForm.value)
+        }
+    }
 }
